22c77793c5523893b1e12df6c68a5be64590f0f5
ajith <coolajith46@gmail.com>
2020-03-26 13:20:41 +0530

    UI Optimizations 2.

--- app/src/main/java/com/ajith/silverprice/MainActivity.java
+++ app/src/main/java/com/ajith/silverprice/MainActivity.java
@@ -1,16 +1,13 @@
 package com.ajith.silverprice;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AlertDialogLayout;
 import androidx.cardview.widget.CardView;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,10 +15,8 @@ public class MainActivity extends AppCompatActivity implements EditDialog.Exampl
 
     private static final String TAG = "ajju";
     EditText old_weight, new_weight, kooli;
-    TextView price, new_w, old_w, kooli_value, total;
+    TextView price, pay, left, reduced, wage, total;
     private CardView cardView;
-
-    ProgressDialog p;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,38 +28,33 @@ public class MainActivity extends AppCompatActivity implements EditDialog.Exampl
         kooli = findViewById(R.id.kooli);
 
         price = findViewById(R.id.price);
-        new_w = findViewById(R.id.weight_new);
-        old_w = findViewById(R.id.old_w);
-        kooli_value = findViewById(R.id.wage);
+        pay = findViewById(R.id.pay);
+        left = findViewById(R.id.left);
+        reduced = findViewById(R.id.reduced);
+        wage = findViewById(R.id.wage);
         total = findViewById(R.id.total);
         cardView = findViewById(R.id.card);
 
-        p = new ProgressDialog(this);
     }
 
     public void calculate(View view) {
 
-        //p.show();
-
-        double w_n=0, w_o=0;
-        double k=0;
+        float w_n=0, w_o=0;
+        float k=0;
 
         if (!TextUtils.isEmpty(new_weight.getText())){
-            w_n = Double.parseDouble(new_weight.getText().toString());
+            w_n = Float.parseFloat(new_weight.getText().toString());
 
             if (!TextUtils.isEmpty(new_weight.getText())){
-                w_o = Double.parseDouble(old_weight.getText().toString());
+                w_o = Float.parseFloat(old_weight.getText().toString());
 
                 if (!TextUtils.isEmpty(new_weight.getText())){
-                    k = Double.parseDouble(kooli.getText().toString());
+                    k = Float.parseFloat(kooli.getText().toString());
 
                     String t_f = findValue(w_n, k, w_o);
                     if (t_f == null){
                         Toast.makeText(this, "Please enter Silver rate", Toast.LENGTH_SHORT).show();
                     }else {
-                        new_w.setText(new_weight.getText().toString());
-                        old_w.setText(old_weight.getText().toString());
-                        kooli_value.setText(kooli.getText().toString());
                         total.setText(t_f);
                     }
                 }else {
@@ -80,17 +70,23 @@ public class MainActivity extends AppCompatActivity implements EditDialog.Exampl
         //p.hide();
     }
 
-    String findValue(double new_wt, double wage, double old){
-
-        Log.d(TAG, "findValue: ");
+    String findValue(float new_wt, float wage1, float old){
 
         double price1 = Integer.parseInt(price.getText().toString());
 
         if (price1 == 0){
             return null;
         }else {
+
+            Toast.makeText(this, String.valueOf((new_wt - (0.68 * old))), Toast.LENGTH_SHORT).show();
+
             cardView.setVisibility(View.VISIBLE);
-            double total = ((price1/10) * (new_wt - (68%old))) + wage;
+            double a = 0.68 * old;
+            left.setText(String.valueOf(a));
+            pay.setText(String.valueOf(new_wt - a));
+            reduced.setText(String.valueOf(old - a));
+            wage.setText(String.valueOf(wage1));
+            double total = ((price1/10) * (new_wt - a)) + wage1;
             return String.valueOf(total);
         }
     }
